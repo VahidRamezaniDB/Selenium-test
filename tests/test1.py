7245bb771cc1c6fab32e49f486fc91fc96a1b5cd
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-06-29 17:35:32 +0430

    test1 modified to new approach

--- tests/test1.py
+++ tests/test1.py
@@ -1,8 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+TEST_DESCRIPTION = "Test #1\nThis test case checks first page element.\n"
+
 driver = webdriver.Chrome()
 driver.get("https://www.partsoftware.com/")
 elem = driver.find_element(By.XPATH, "/html/body/div/main/section[1]/div[2]/h1")
-assert "طــراح" in elem.text
-driver.close()
+try:
+    assert "طــراح" in elem.text
+    print("passed.")
+    print(TEST_DESCRIPTION)
+except AssertionError as e:
+    print("rejected")
+finally:
+    driver.close()    
