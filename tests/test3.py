ba0f40849da13e0b335f3500629f042353fea5da
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-06-29 19:18:07 +0430

    first page tests

--- tests/test3.py
+++ tests/test3.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-TEST_DESCRIPTION = "Test #1\nThis test case checks first page element.\n\n"
+TEST_DESCRIPTION = "Test #3\nThis test case checks first page elements.\n\n"
 DELIMITER = "-----------------------------------------------------\n\n"
 PASSED = "Passed.\n\n"
 FAILED = "Failed.\n\n"
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get("https://www.partsoftware.com/")
-elem = driver.find_element(By.XPATH, "/html/body/div/main/section[1]/div[2]/h1")
+elem = driver.find_element(By.XPATH, "/html/body/div/nav/div/ul/li[2]/a")
 try:
-    assert "طــراح ســـامـانه‌های پـــردازش اطــلاعات مـــالــی" in elem.text
+    assert "درباره ما" in elem.text
     with open("OutLog.txt", 'at') as out_file:
         out_file.write(TEST_DESCRIPTION)
         out_file.write(PASSED)
--- tests/test9.py
+++ tests/test9.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-TEST_DESCRIPTION = "Test #1\nThis test case checks first page element.\n\n"
+TEST_DESCRIPTION = "Test #9\nThis test case checks the down-arrow element in the first page.\n\n"
 DELIMITER = "-----------------------------------------------------\n\n"
 PASSED = "Passed.\n\n"
 FAILED = "Failed.\n\n"
 
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get("https://www.partsoftware.com/")
-elem = driver.find_element(By.XPATH, "/html/body/div/main/section[1]/div[2]/h1")
+elem = driver.find_element(By.XPATH, "/html/body/div/main/div[1]/div/div/a/figure")
 try:
-    assert "طــراح ســـامـانه‌های پـــردازش اطــلاعات مـــالــی" in elem.text
+    assert elem.is_displayed() == True
     with open("OutLog.txt", 'at') as out_file:
         out_file.write(TEST_DESCRIPTION)
         out_file.write(PASSED)
